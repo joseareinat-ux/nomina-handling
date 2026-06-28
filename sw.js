729a4c78f5d7f7a5594fa659ac7e821780781749
joseareinat-ux <joseareinat@gmail.com>
2026-06-28 11:58:12 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,32 @@
-const CACHE = 'nomina-v1';
-const FILES = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'nomina-v2';
+const FILES = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+  self.skipWaiting();
 });
 
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Estrategia "network-first": siempre intenta cargar la version mas reciente.
+// Si no hay conexion, usa la copia guardada en cache.
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    fetch(e.request)
+      .then(res => {
+        const resClone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, resClone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
   );
 });
